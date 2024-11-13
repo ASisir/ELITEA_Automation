2342f54107472acb98386b9d1c9878ee794c0f74
ASisir <klucse2000031490@gmail.com>
2024-11-13 15:38:13 +0530

    Create ELITEADemo/PageObjects/LoginPage.cs

---
+++ ELITEADemo/PageObjects/LoginPage.cs
@@ -0,0 +1,33 @@
+using OpenQA.Selenium;
+
+namespace ELITEADemo.PageObjects
+{
+    public class LoginPage
+    {
+        private IWebDriver driver;
+
+        private By usernameField = By.XPath("//input[@id='user-name']");
+        private By passwordField = By.XPath("//input[@id='password']");
+        private By loginButton = By.XPath("//input[@id='login-button']");
+
+        public LoginPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public void EnterUsername(string username)
+        {
+            driver.FindElement(usernameField).SendKeys(username);
+        }
+
+        public void EnterPassword(string password)
+        {
+            driver.FindElement(passwordField).SendKeys(password);
+        }
+
+        public void ClickLogin()
+        {
+            driver.FindElement(loginButton).Click();
+        }
+    }
+}
