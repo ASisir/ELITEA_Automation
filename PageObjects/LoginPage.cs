00f5a841889b880682dbba8e6ee0591396dc9060
ASisir <klucse2000031490@gmail.com>
2024-11-14 18:14:26 +0530

    Update PageObjects/LoginPage.cs

--- PageObjects/LoginPage.cs
+++ PageObjects/LoginPage.cs
@@ -1,29 +1,32 @@
 using OpenQA.Selenium;
 
-public class LoginPage
+namespace ELITEADemo.PageObjects
 {
-    private readonly IWebDriver _driver;
-    private readonly By _usernameField = By.XPath("//input[@id='user-name']");
-    private readonly By _passwordField = By.XPath("//input[@id='password']");
-    private readonly By _loginButton = By.XPath("//input[@id='login-button']");
-
-    public LoginPage(IWebDriver driver)
+    public class LoginPage
     {
-        _driver = driver;
-    }
+        private IWebDriver driver;
+        private By usernameField = By.XPath("//input[@id='user-name']");
+        private By passwordField = By.XPath("//input[@id='password']");
+        private By loginButton = By.XPath("//input[@id='login-button']");
 
-    public void EnterUsername(string username)
-    {
-        _driver.FindElement(_usernameField).SendKeys(username);
-    }
+        public LoginPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
 
-    public void EnterPassword(string password)
-    {
-        _driver.FindElement(_passwordField).SendKeys(password);
-    }
+        public void EnterUsername(string username)
+        {
+            driver.FindElement(usernameField).SendKeys(username);
+        }
 
-    public void ClickLogin()
-    {
-        _driver.FindElement(_loginButton).Click();
+        public void EnterPassword(string password)
+        {
+            driver.FindElement(passwordField).SendKeys(password);
+        }
+
+        public void ClickLoginButton()
+        {
+            driver.FindElement(loginButton).Click();
+        }
     }
 }
